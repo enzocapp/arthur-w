e9b181caa50444ed4e2f52bd2228979a55c1f125
enzocapp <76573212+enzocapp@users.noreply.github.com>
2023-03-04 18:57:01 -0300

    Add files via upload

--- src/model/client.js
+++ src/model/client.js
@@ -1,15 +1,6 @@
 const mongoose = require("mongoose");
 const { Schema, model } = require("mongoose");
 const cli = require("nodemon/lib/cli");
-const url =
-  "mongodb+srv://Enzo:UDRDRiI6l0NMmj0G@arthur-workshop.bwnuexp.mongodb.net/database-aw?retryWrites=true&w=majority";
-
-mongoose
-  .connect(url)
-  .then(() => console.log("SE HA CONECTADO A MONGODB"))
-  .catch((e) =>
-    console.error("No se conecto a MONGODB debido a este error:" + e)
-  );
 
 const clientSchema = new Schema(
   {
@@ -17,6 +8,7 @@ const clientSchema = new Schema(
     products: String,
     telNumber: Number,
     adress: String,
+    done: { type: Boolean, default: false }
   },
   { timestamps: true, versionKey: false }
 );
